e8db21fffd102fd1606fd6eb63da1ab07318dae0
IvanQM19 <85361546+IvanQM19@users.noreply.github.com>
2021-07-05 09:59:47 -0500

    Add files via upload

---
+++ ExamenPrimerEjercicio.m
@@ -0,0 +1,41 @@
+# Created by Octave 6.2.0, Mon Jul 05 09:58:43 2021 GMT <unknown@LAPTOP-GJ7GVF4S>
+# name: Area
+# type: scalar
+79.404191999999867
+
+
+# name: a
+# type: scalar
+-2
+
+
+# name: b
+# type: scalar
+5
+
+
+# name: base_incrementox
+# type: scalar
+0.014
+
+
+# name: f
+# type: scalar
+24.999999999999961
+
+
+# name: i
+# type: scalar
+499
+
+
+# name: n
+# type: scalar
+500
+
+
+# name: x
+# type: scalar
+4.9999999999999902
+
+
